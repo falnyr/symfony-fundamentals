d49290966aeb1445f4428b1fb56d5b3a7d30de0f
Jan Richter <falnyr@gmail.com>
2021-05-09 12:58:11 +1200

    feat: binding a service explicitly - adding parameters

--- src/Service/MarkDownHelper.php
+++ src/Service/MarkDownHelper.php
@@ -9,15 +9,25 @@ class MarkDownHelper
 {
     private MarkdownParserInterface $markdownParser;
     private CacheInterface $cache;
+    private bool $isDebug;
 
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache)
-    {
+    public function __construct(
+        MarkdownParserInterface $markdownParser,
+        CacheInterface $cache,
+        bool $isDebug
+    ) {
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        dump($isDebug);
     }
 
     public function parse(string $source): string
     {
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+
         return $this->cache->get('markdown_'.md5($source), fn() => $this->markdownParser->transformMarkdown($source));
     }
 }
